3d38963f0d45aee94e8a94142822b5b3d43ec1b7
SergioAristides <sergioa.riverab@autonoma.edu.co>
2023-10-26 14:07:36 -0500

    url especificas por id

--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\apii\v1;
-
+namespace App\Http\Controllers\api\v1;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\api\v1\UserStoreRequest;
+use App\Http\Resources\api\v1\UserShowResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,15 +11,40 @@ class UserController extends Controller
 {
 
 
-    public function usersComputers(){
+
+    public function usersComputers(String $id){
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        // Cargar los computadores relacionados
+        $user->load('computers');
+
+        return response()->json(['data' => $user], 200);
+    }
+
+    public function usersComputersAll(){
         $users = User::orderBy('name','asc')->with('computers')->get();
         return response()->json(['data'=>$users],200);
 
     }
 
-    public function usersObservations(){
+    public function usersObservationsAll(){
         $users = User::orderBy('name','asc')->with('observations')->get();
          return response()->json(['data'=>$users],200);
+    }
+
+    public function userObservations(string $id){
+        $user=User::find($id);
+        if(!$user){
+            return response()->json(['message'=>'usuario no encontrado'],404);
+        }
+
+        $user->load('observations');
+        return response()->json(['data'=>$user],200);
+
     }
     /**
      * Display a listing of the resource.
@@ -27,13 +53,13 @@ class UserController extends Controller
     {
         //
         $users = User::orderBy('name','asc')->get();
-        return response()->json(['data'=>$users],200);
+        return response()->json(['data'=>UserShowResource::collection($users)],200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserStoreRequest $request)
     {
         //
         $user=User::create($request->all());
@@ -48,7 +74,7 @@ class UserController extends Controller
     {
         //
 
-        return response()->json(['data'=>$user],200);
+        return response()->json(['data'=>new UserShowResource($user)],200);
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -22,25 +22,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //ruta crud de usuarios
 
 Route::get('/v1/users/computers',
-[App\Http\Controllers\apii\v1\UserController::class ,'usersComputers']);
+[App\Http\Controllers\api\v1\UserController::class ,'usersComputersAll']);
+
+Route::get('/v1/users/{id}/computers',
+ [App\Http\Controllers\api\v1\UserController::class, 'usersComputerss']);
+
 Route::get('/v1/users/observations',
-[App\Http\Controllers\apii\v1\UserController::class ,'usersObservations']);
+[App\Http\Controllers\api\v1\UserController::class ,'usersObservationsAll']);
+
+Route::get('/v1/users/{id}/observations',
+[App\Http\Controllers\api\v1\UserController::class ,'userObservations']);
+
+
 Route::apiResource('v1/users'
-,App\Http\Controllers\apii\v1\UserController::class);
+,App\Http\Controllers\api\v1\UserController::class);
 
 Route::get('/v1/computers/observations',
-[App\Http\Controllers\apii\v1\ComputerController::class ,'computersObservations']);
+[App\Http\Controllers\api\v1\ComputerController::class ,'computersObservations']);
 Route::apiResource('v1/computers'
-,App\Http\Controllers\apii\v1\ComputerController::class);
-
+,App\Http\Controllers\api\v1\ComputerController::class);
 
 Route::apiResource('v1/observations'
-,App\Http\Controllers\apii\v1\ObservationController::class);
+,App\Http\Controllers\api\v1\ObservationController::class);
 
 
 Route::get('/v1/categories/observations',
-[App\Http\Controllers\apii\v1\CategoryController::class,'categoriesObservations']);
+[App\Http\Controllers\api\v1\CategoryController::class,'categoriesObservations']);
 Route::apiResource('v1/categories'
-,App\Http\Controllers\apii\v1\CategoryController::class);
-
+,App\Http\Controllers\api\v1\CategoryController::class);
 
